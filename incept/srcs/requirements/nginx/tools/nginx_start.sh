b6c96961061c234d8b2ec2216d3ece42d8a76ad4
Bahadır Durmuş <mrbahadir25@gmail.com>
2023-10-25 20:09:14 +0300

    a

--- incept/srcs/requirements/nginx/tools/nginx_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
-	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=TR/ST=KOCAELI/L=GEBZE/O=42Kocaeli/CN=bdurmus.42.fr";
-fi
-
-exec "$@"
